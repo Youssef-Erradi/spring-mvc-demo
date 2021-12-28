cc5e6149745144606c00a949ccb219c90a552d1a
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2021-12-28 16:47:32 +0100

    initial commit

---
+++ src/main/java/org/sim/SpringMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package org.sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sim/SpringMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
