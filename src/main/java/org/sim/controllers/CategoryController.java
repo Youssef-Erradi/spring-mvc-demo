b092ed099e03132c4977d4c85d3c215fcff5c214
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-01-03 00:20:27 +0100

    add RequestParams to filter results

--- src/main/java/org/sim/controllers/CategoryController.java
+++ src/main/java/org/sim/controllers/CategoryController.java
@@ -1,5 +1,7 @@
 package org.sim.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.sim.dao.CategoryRepository;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -21,8 +24,11 @@ public class CategoryController {
 	private final String folderName = "categories";
 	
 	@GetMapping("")
-	public String index(Model model) {
-		model.addAttribute("categories", repository.findAll());
+	public String index(@RequestParam(defaultValue = "") String description,
+						@RequestParam(defaultValue = "") String name, Model model) {
+		List<Category> categories = repository.findByDescriptionContaining(description);
+		categories.retainAll(repository.findByNameContaining(name));
+		model.addAttribute("categories",categories);
 		return folderName+"/list";
 	}
 	
@@ -43,7 +49,8 @@ public class CategoryController {
 	}
 	
 	@PostMapping("/save")
-	public String save(@Valid Category category, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+	public String save(@Valid Category category, BindingResult bindingResult,
+						Model model,RedirectAttributes redirectAttributes) {
 		if(bindingResult.hasErrors()) {
 			model.addAttribute("message", "There is some errors during validation");
 			model.addAttribute("category", category);
--- src/main/java/org/sim/controllers/ProductController.java
+++ src/main/java/org/sim/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package org.sim.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,8 +29,15 @@ public class ProductController {
 	private final String folderName = "products";
 	
 	@GetMapping("")
-	public String index(Model model) {
-		model.addAttribute("products", repository.findAll());
+	public String index(@RequestParam(defaultValue = "") String description,
+						@RequestParam(defaultValue = "") String name,
+						@RequestParam(defaultValue = "0") double min,
+						@RequestParam(defaultValue = Double.MAX_VALUE+"") double max,
+						Model model) {
+		List<Product> products = repository.findByDescriptionContaining(description);
+		products.retainAll(repository.findByNameContaining(name));
+		products.retainAll(repository.findByPriceBetween(min, max));
+		model.addAttribute("products", products);
 		return folderName+"/list";
 	}
 	
