78a62c106808435f3db5bb4dd1adbfdf8f53e3e3
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-01-02 23:53:04 +0100

    add findBy methods to JPA Repos

--- src/main/java/org/sim/dao/CategoryRepository.java
+++ src/main/java/org/sim/dao/CategoryRepository.java
@@ -1,8 +1,11 @@
 package org.sim.dao;
 
+import java.util.List;
+
 import org.sim.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
+	public List<Category> findByDescriptionContaining(String description);
+	public List<Category> findByNameContaining(String name);
 }
--- src/main/java/org/sim/dao/ProductRepository.java
+++ src/main/java/org/sim/dao/ProductRepository.java
@@ -1,8 +1,12 @@
 package org.sim.dao;
 
+import java.util.List;
+
 import org.sim.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+	public List<Product> findByDescriptionContaining(String description);
+	public List<Product> findByNameContaining(String name);
+	public List<Product> findByPriceBetween(double start, double end);
 }
